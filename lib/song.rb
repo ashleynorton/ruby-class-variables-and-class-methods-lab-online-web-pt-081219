bc3b983b2a613956f028b4aefe710e8970f6eb17
Ashley Norton <nortonashleym@gmail.com>
2019-09-25 00:36:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
   def self.genre_count
     genre_hash = Hash.new 
     @@genres.each do |genre|
-      if genre.uniq 
+      if genre
         genre_hash[genre] += 1 
       end  
     end    
